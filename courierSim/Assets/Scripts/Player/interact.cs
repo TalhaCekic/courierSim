631fbfb68cc909bbaf3bfb9349f6286a101186aa
Myso <talhacekicc123@gmail.com>
2023-12-03 00:04:59 +0300

    h

--- courierSim/Assets/Scripts/Player/interact.cs
+++ courierSim/Assets/Scripts/Player/interact.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class interact : MonoBehaviour
 {
     [SerializeField] public int maxDistance;
     private PlayerInput playerInput;
 
+     public Image interactImage; 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -25,6 +28,16 @@ public class interact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+       
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
+            interactImage.color = Color.green;
+        }
     }
 
     public void Interact(InputAction.CallbackContext context)
