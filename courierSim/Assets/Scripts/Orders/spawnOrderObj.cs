2a55427af845dec64dc7e5fabe5f710b7755109c
Myso <talhacekicc123@gmail.com>
2023-12-20 21:22:01 +0300

    l

--- courierSim/Assets/Scripts/Orders/spawnOrderObj.cs
+++ courierSim/Assets/Scripts/Orders/spawnOrderObj.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class spawnOrderObj : MonoBehaviour
 {
     public Image[] pp;
+    public Image selectedPP;
     void Start()
     {
         
@@ -14,5 +17,31 @@ public class spawnOrderObj : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void orderSpawn()
+    {
+        if (OrderManager.instance.isSpawn)
+        {
+            int randomIndex = Random.Range(0, pp.Length);
+            for (int i = 0; i < pp.Length; i++)
+            {
+                pp[randomIndex].gameObject.SetActive(true);
+            }
+        }
+       
+    }
+    //randomize aray sistemi
+    private T GetRandomElement<T>(T[] array)
+    {
+        if (array != null && array.Length > 0)
+        {
+            int randomIndex = Random.Range(0, array.Length);
+            return array[randomIndex];
+        }
+        else
+        {
+            return default(T);
+        }
     }
 }
