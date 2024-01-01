02d42f8976fe1cb3a6624b92ee3e414653b7df53
Myso <talhacekicc123@gmail.com>
2024-01-01 22:19:41 +0300

    a

--- courierSim/Assets/Scripts/phone/navmeshMap.cs
+++ courierSim/Assets/Scripts/phone/navmeshMap.cs
@@ -7,63 +7,71 @@ public class navmeshMap : MonoBehaviour
     public LineRenderer lineRenderer;
 
     private NavMeshPath navMeshPath;
-    private NavMeshAgent navMeshAgent;
-    
+
     public const int CustomAreaIndex = 1;
 
     void Start()
     {
         navMeshPath = new NavMeshPath();
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = null;
         DrawPath();
-    }
-
-    private void SelectTarget()
-    {
-        if (OrderManager.instance.isBurger && !interact.instance.isBurgerYes)
-        {
-            target = BurgerShop.instance.Target;
-        } 
-        if (OrderManager.instance.isPizza && !interact.instance.isPizzaYes)
-        {
-            target = PizzaShop.instance.Target;
-        }
-    }
-    void DrawPath()
-    {
-        SelectTarget();
-        if (target == null)
-            return;
-        
-        int customAreaMask = 1 << CustomAreaIndex;
-        int navMeshAreaMask = NavMesh.AllAreas & ~customAreaMask;
-        
-        navMeshAgent.SetDestination(target.position);
-        NavMesh.CalculatePath(transform.position, target.position, navMeshAreaMask, navMeshPath);
-
-        if (navMeshPath.status != NavMeshPathStatus.PathComplete)
-        {
-            Debug.LogWarning("Path not complete");
-            return;
-        }
-        lineRenderer.positionCount = 0;
-
-        for (int i = 0; i < navMeshPath.corners.Length; i++)
-        {
-            print("girse");
-            AddPointToLineRenderer(navMeshPath.corners[i]);
-        }
-    }
-
-    void AddPointToLineRenderer(Vector3 point)
-    {
-        // Line Renderer'a nokta ekleyin
-        lineRenderer.positionCount++;
-        lineRenderer.SetPosition(lineRenderer.positionCount - 1, point);
     }
 
     void Update()
     {
         DrawPath();
+    }
+
+    private void SelectTarget()
+    {
+        if (OrderManager.instance.isOrder)
+        {
+            lineRenderer.enabled = true;
+            if (OrderManager.instance.isBurger && !interact.instance.isBurgerYes)
+            {
+                target = BurgerShop.instance.Target;
+            }
+            else if (OrderManager.instance.isBurger && interact.instance.isBurgerYes)
+            {
+                target = OrderManager.instance.selectedDeliveryPosition.transform;
+            }
+
+            if (OrderManager.instance.isPizza && !interact.instance.isPizzaYes)
+            {
+                target = PizzaShop.instance.Target;
+            }
+            else if (OrderManager.instance.isPizza && interact.instance.isPizzaYes)
+            {
+                target = OrderManager.instance.selectedDeliveryPosition.transform;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+
+    void DrawPath()
+    {
+        SelectTarget();
+        if (target != null)
+        {
+            int customAreaMask = 1 << CustomAreaIndex;
+            int navMeshAreaMask = NavMesh.AllAreas & ~customAreaMask;
+
+            NavMesh.CalculatePath(transform.position, target.position, navMeshAreaMask, navMeshPath);
+            lineRenderer.positionCount = 0;
+            
+            for (int i = 0; i < navMeshPath.corners.Length; i++)
+            {
+                AddPointToLineRenderer(navMeshPath.corners[i]);
+            }
+        }
+    }
+
+    void AddPointToLineRenderer(Vector3 point)
+    {
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, point);
     }
 }
